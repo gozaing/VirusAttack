a93f66dbfea49a86991ad9284f49466bbb9cea19
HideyasuTobaru <steganographi@gmail.com>
2015-06-10 23:07:49 +0900

    add sample @2x image

--- VirusAttack/MenuViewController.swift
+++ VirusAttack/MenuViewController.swift
@@ -11,27 +11,27 @@ import UIKit
 
 class MenuViewController: UIViewController {
 
-//    private var myImageView: UIImageView!
+    private var myImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        
-//        // UIImageViewを作成する.
+        // 表示する画像を設定する.
+        let myImage = UIImage(named: "test_iphone6")
+
+        // UIImageViewを作成する.
 //        myImageView = UIImageView(frame: CGRectMake(0,0,750,1334))
-//        
-//        // 表示する画像を設定する.
-//        let myImage = UIImage(named: "test_@1")
-//        
-//        // 画像をUIImageViewに設定する.
-//        myImageView.image = myImage
-//        
-//        // 画像の表示する座標を指定する.
-//        myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height/2)
-//        
-//        // UIImageViewをViewに追加する.
-//        self.view.addSubview(myImageView)
-//        
+        myImageView = UIImageView(frame: CGRectMake(0,0,myImage!.size.width,myImage!.size.height))
+        
+        // 画像をUIImageViewに設定する.
+        myImageView.image = myImage
+        
+        // 画像の表示する座標を指定する.
+        myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height/2)
+        
+        // UIImageViewをViewに追加する.
+        self.view.addSubview(myImageView)
+        
 
         
 
