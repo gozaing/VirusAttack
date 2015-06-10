b1b00307990d798f4bc538a04726315b33729221
HideyasuTobaru <steganographi@gmail.com>
2015-06-10 22:24:33 +0900

    sample view image

--- VirusAttack/MenuViewController.swift
+++ VirusAttack/MenuViewController.swift
@@ -10,9 +10,31 @@ import Foundation
 import UIKit
 
 class MenuViewController: UIViewController {
+
+//    private var myImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        
+//        // UIImageViewを作成する.
+//        myImageView = UIImageView(frame: CGRectMake(0,0,750,1334))
+//        
+//        // 表示する画像を設定する.
+//        let myImage = UIImage(named: "test_@1")
+//        
+//        // 画像をUIImageViewに設定する.
+//        myImageView.image = myImage
+//        
+//        // 画像の表示する座標を指定する.
+//        myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height/2)
+//        
+//        // UIImageViewをViewに追加する.
+//        self.view.addSubview(myImageView)
+//        
+
+        
+
     }
     
     @IBAction func backFromSecondView(segue:UIStoryboardSegue){
