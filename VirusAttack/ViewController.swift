7511b793a2a55b87c8e88a4f9bb9bf8b99ce0212
HideyasuTobaru <steganographi@gmail.com>
2015-06-14 21:41:55 +0900

    add loadView method before view controller viewdidload

--- VirusAttack/ViewController.swift
+++ VirusAttack/ViewController.swift
@@ -13,39 +13,55 @@ let homeIcon = UIButton(frame: CGRectMake(50, 400, 200, 50))
 
 class ViewController: UIViewController {
     
+    var viewInitiated: Bool = false
+    
+    override func loadView() {
+        let skView = SKView()
+        self.view = skView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        NSLog("start ViewController")
-        
-        // シーン作成
-        let scene = GameScene()
-        // SKView型で取り出す
-        let view = self.view as! SKView
-        
-        // debug info
-        view.showsFPS = true
-        view.showsNodeCount = true
-        
-        // シーンのサイズをビューに合わせる
-        scene.size = view.frame.size
-        
-        // background
-        let backGroundTexture = SKTexture(imageNamed: "bath_750_1334")
-        let background = SKSpriteNode(texture: backGroundTexture)
-        background.name = "background"
-        background.position = CGPoint(x: scene.size.width/2 , y: scene.size.height/2)
-        scene.addChild(background)
-        
-        // ビューの上にシーンを作成
-        view.presentScene(scene)
-        
-        // ボタンを生成
-        homeIcon.setImage(UIImage(named: "home"), forState: .Normal)
-        homeIcon.addTarget(self, action: "onClickHomeIcon:", forControlEvents: .TouchUpInside)
-        homeIcon.alpha = CGFloat(0)
-        self.view.addSubview(homeIcon)
-        
+
+    }
+    
+    override func viewWillLayoutSubviews() {
+        if(!viewInitiated){
+            super.viewWillLayoutSubviews()
+
+            NSLog("start ViewController")
+            
+            // シーン作成
+            let scene = GameScene()
+            // SKView型で取り出す
+            //        let view = self.view as! SKView
+            let view = self.view as! SKView
+            
+            // debug info
+            view.showsFPS = true
+            view.showsNodeCount = true
+            
+            // シーンのサイズをビューに合わせる
+            scene.size = view.frame.size
+            
+            // background
+            let backGroundTexture = SKTexture(imageNamed: "bath_750_1334")
+            let background = SKSpriteNode(texture: backGroundTexture)
+            background.name = "background"
+            background.position = CGPoint(x: scene.size.width/2 , y: scene.size.height/2)
+            scene.addChild(background)
+            
+            // ビューの上にシーンを作成
+            view.presentScene(scene)
+            
+            // ボタンを生成
+            homeIcon.setImage(UIImage(named: "home"), forState: .Normal)
+            homeIcon.addTarget(self, action: "onClickHomeIcon:", forControlEvents: .TouchUpInside)
+            homeIcon.alpha = CGFloat(0)
+            self.view.addSubview(homeIcon)
+
+            self.viewInitiated = true
+        }
     }
 
     override func didReceiveMemoryWarning() {
