51bede474aa4fc0b8b2f8b06a6e64710c23e0705
HideyasuTobaru <steganographi@gmail.com>
2015-06-21 12:50:21 +0900

    add menu icon

--- VirusAttack/MenuViewController.swift
+++ VirusAttack/MenuViewController.swift
@@ -27,26 +27,59 @@ class MenuViewController: UIViewController {
         self.view.sendSubviewToBack(menuImageView)
 
 
-        // game start button
-        let gameStartButton: UIButton = UIButton(frame: CGRectMake(0,0,200,200))
-        gameStartButton.backgroundColor = UIColor.redColor();
-        gameStartButton.layer.masksToBounds = true
-        gameStartButton.setTitle("start", forState: .Normal)
-        gameStartButton.layer.cornerRadius = 20.0
-        gameStartButton.layer.position = CGPoint(x: self.view.bounds.width/2 , y:self.view.bounds.height*0.75)
-        gameStartButton.addTarget(self, action: "onClickStartGame:", forControlEvents: .TouchUpInside)
-
-        gameStartButton.setImage(UIImage(named: "cake_1"), forState: .Normal)
-        // ボタンを追加する.
-        self.view.addSubview(gameStartButton)
+        // game start button 1
+        let gameStartButton1: UIButton = UIButton(frame: CGRectMake(0,0,150,150))
+        gameStartButton1.backgroundColor = UIColor.redColor();
+        gameStartButton1.layer.masksToBounds = true
+        gameStartButton1.setTitle("start", forState: .Normal)
+        gameStartButton1.layer.cornerRadius = 20.0
+        gameStartButton1.layer.position = CGPoint(x: (self.view.bounds.width/4)*1 , y:self.view.bounds.height*0.35)
+        gameStartButton1.addTarget(self, action: "onClickStartGame:", forControlEvents: .TouchUpInside)
         
+        gameStartButton1.setImage(UIImage(named: "cake_1"), forState: .Normal)
+        self.view.addSubview(gameStartButton1)
+
+        // game start button 2
+        let gameStartButton2: UIButton = UIButton(frame: CGRectMake(0,0,150,150))
+        gameStartButton2.backgroundColor = UIColor.redColor();
+        gameStartButton2.layer.masksToBounds = true
+        gameStartButton2.setTitle("start", forState: .Normal)
+        gameStartButton2.layer.cornerRadius = 20.0
+        gameStartButton2.layer.position = CGPoint(x: (self.view.bounds.width/4)*3 , y:self.view.bounds.height*0.35)
+        gameStartButton2.addTarget(self, action: "onClickStartGame:", forControlEvents: .TouchUpInside)
+        gameStartButton2.setImage(UIImage(named: "cake_1"), forState: .Normal)
+        self.view.addSubview(gameStartButton2)
+
+        // game start button 3
+        let gameStartButton3: UIButton = UIButton(frame: CGRectMake(0,0,150,150))
+        gameStartButton3.backgroundColor = UIColor.redColor();
+        gameStartButton3.layer.masksToBounds = true
+        gameStartButton3.setTitle("start", forState: .Normal)
+        gameStartButton3.layer.cornerRadius = 20.0
+        gameStartButton3.layer.position = CGPoint(x: (self.view.bounds.width/4)*1 , y:self.view.bounds.height*0.60)
+        gameStartButton3.addTarget(self, action: "onClickStartGame:", forControlEvents: .TouchUpInside)
+        
+        gameStartButton3.setImage(UIImage(named: "cake_1"), forState: .Normal)
+        self.view.addSubview(gameStartButton3)
+        
+        // game start button 4
+        let gameStartButton4: UIButton = UIButton(frame: CGRectMake(0,0,150,150))
+        gameStartButton4.backgroundColor = UIColor.redColor();
+        gameStartButton4.layer.masksToBounds = true
+        gameStartButton4.setTitle("start", forState: .Normal)
+        gameStartButton4.layer.cornerRadius = 20.0
+        gameStartButton4.layer.position = CGPoint(x: (self.view.bounds.width/4)*3 , y:self.view.bounds.height*0.60)
+        gameStartButton4.addTarget(self, action: "onClickStartGame:", forControlEvents: .TouchUpInside)
+        gameStartButton4.setImage(UIImage(named: "cake_1"), forState: .Normal)
+        self.view.addSubview(gameStartButton4)
+
         // tutorial view
         let helpButton: UIButton = UIButton(frame: CGRectMake(0,0,100,100))
         helpButton.backgroundColor = UIColor.grayColor();
         helpButton.layer.masksToBounds = true
         helpButton.setTitle("help", forState: .Normal)
         helpButton.layer.cornerRadius = 20.0
-        helpButton.layer.position = CGPoint(x: self.view.bounds.width/2 , y:self.view.bounds.height*0.5)
+        helpButton.layer.position = CGPoint(x: self.view.bounds.width/2 , y:self.view.bounds.height*0.9)
         helpButton.addTarget(self, action: "onClickHelpMenu:", forControlEvents: .TouchUpInside)
 //        helpButton.setImage(UIImage(named: "cake_1"), forState: .Normal)
         // ボタンを追加する.
